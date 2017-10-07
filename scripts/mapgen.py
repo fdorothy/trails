ab77c5638ea0b7aeb7c88b3873c6de18348b7bf1
Fredric Dorothy <fredricdorothy@gmail.com>
2017-10-07 11:07:03 -0500

    added some random paths

--- scripts/mapgen.py
+++ scripts/mapgen.py
@@ -1,11 +1,13 @@
 #/bin/bash python
 
+from random import shuffle
 import logging
 import json
 import noise
 import curses
 import math
 import numpy as np
+import sets
 
 global logger
 
@@ -65,7 +67,7 @@ class Elevation(Layer):
   def display(self, i, j):
     z = self.data[j,i]
     z = int((z+1.0) * 5.0)
-    return (str(z) + '.', curses.color_pair(z))
+    return ('..', curses.color_pair(z))
 
 class Contour(Layer):
   def __init__(self, elevation, val, name = "contour"):
@@ -83,7 +85,114 @@ class Contour(Layer):
 
   def display(self, i, j):
     if self.data[j,i]:
-      return ('..', curses.color_pair(1))
+      return ('..', curses.color_pair(0))
+    else:
+      return None
+
+class Path(Layer):
+  def __init__(self, elevation, start, end, name = "path"):
+    self.elevation = elevation
+    self.start = start
+    self.end = end
+    self.data = None
+    super(Path, self).__init__(self.elevation.size, name)
+
+  def neighbors(self, pos):
+    x,y = pos
+    return [
+      (x-1, y-1),
+      (x, y-1),
+      (x+1, y-1),
+      (x-1, y),
+      (x+1, y),
+      (x-1, y+1),
+      (x, y+1),
+      (x+1, y+1)
+    ]
+
+  def lowest_score(self, openSet, fScore):
+    mink = None
+    minv = 0
+    for k in openSet:
+      v = fScore[k]
+      if mink == None or v < minv:
+        mink = k
+        minv = v
+    return (mink, minv)
+
+  def dist_between(self, p1, p2):
+    z1 = self.elevation.data[p1[1],p1[0]]
+    z1 = int((1.0+z1)*5.0)
+    z2 = self.elevation.data[p2[1],p2[0]]
+    z2 = int((1.0+z2)*5.0)
+    path = self.data[p2[1],p2[0]]
+    d = math.sqrt(float(p1[0]-p2[0])**2 + float(p1[1]-p2[1])**2 + 10*float(z2-z1)**2)
+    if path == 1:
+      return d*0.75
+    else:
+      return d
+
+  def heuristic_cost_estimate(self, start, end):
+    return self.dist_between(start, end)
+
+  def reconstruct_path(self, cameFrom, current):
+    total_path = [current]
+    while current in cameFrom:
+      current = cameFrom[current]
+      total_path.append(current)
+    return total_path
+
+  def generate(self):
+    if self.data == None:
+      super(Path, self).generate()
+    closedSet = sets.Set([])
+    start = (self.start[0], self.start[1])
+    goal = (self.end[0], self.end[1])
+    openSet = sets.Set([start])
+    cameFrom = {}
+    gScore = {}
+    gScore[start] = 0
+    fScore = {}
+    fScore[start] = self.heuristic_cost_estimate(start, goal)
+
+    while len(openSet) > 0:
+      current, f = self.lowest_score(openSet, fScore)
+      logger.info("visiting %d,%d" % (current[0], current[1]))
+      logger.info("openSet = %s" % str(openSet))
+      if current == goal:
+        path = self.reconstruct_path(cameFrom, current)
+        for (x,y) in path:
+          if self.data[y,x] == 0:
+            self.data[y,x] = 1
+        self.data[path[0][1],path[0][0]] = 2
+        self.data[path[-1][1],path[-1][0]] = 2
+        logger.info("ok, path = %s" % str(path))
+        return True
+      openSet.remove(current)
+      closedSet.add(current)
+      for neighbor in self.neighbors(current):
+        if (neighbor[0] >= 0 and neighbor[1] >= 0 and
+            neighbor[0] < self.size[0] and neighbor[1] < self.size[1]):
+          if neighbor in closedSet:
+            continue
+          openSet.add(neighbor)
+          tentative_gScore = gScore[current] + self.dist_between(current, neighbor)
+          if neighbor in gScore and tentative_gScore >= gScore[neighbor]:
+            continue
+          cameFrom[neighbor] = current
+          gScore[neighbor] = tentative_gScore
+          fScore[neighbor] = gScore[neighbor] + self.heuristic_cost_estimate(neighbor, goal)
+
+    return False
+
+  def display(self, i, j):
+    z = self.data[j,i]
+    if z == 1:
+      x,c = self.elevation.display(i,j)
+      return ('@ ', c)
+    elif z == 2:
+      x,c = self.elevation.display(i,j)
+      return ('X ', c)
     else:
       return None
 
@@ -105,6 +214,16 @@ class World(object):
 def print_world(screen, world):
   curses.start_color()
   curses.use_default_colors()
+  for i in range(1, 11):
+    r,g,b = ((i-1)*100, (i-1)*100, (i-1)*100)
+    if i < 3:
+      b += 300 * i
+    elif i == 3:
+      r += 500
+      g += 500
+    elif i<6:
+      g += 25 * i
+    curses.init_color(i, r, g, b)
   for i in range(0, curses.COLORS):
     curses.init_pair(i, 0, i)
 
@@ -139,11 +258,33 @@ def main(scr):
   elevation.angle = math.radians(-30.0)
   elevation.generate()
 
-  contour = Contour(elevation, 0.0)
-  contour.generate()
+  # random points of interest within the map
+  poi = []
+  pad = 0.25
+  inc = 0.2
+  for i in range(int(size[0]*pad), int(size[0]*(1.0-pad)), int(size[0]*inc)):
+    for j in range(int(size[1]*pad), int(size[1]*(1.0-pad)), int(size[1]*inc)):
+      poi.append((int(i),int(j)))
+  shuffle(poi)
+  poi = poi[0:2]
+
+  # random points of interest to the n, s, e and w
+  poi += [
+    (size[0]/2, 0),
+    (size[0]/2, size[1]-1),
+    (0, size[1]/2),
+    (size[0]-1, size[1]/2)
+  ]
+
+  path = Path(elevation, (0,0), (0,0))
+  for i in range(0,len(poi)-1):
+    for j in range(i+1,len(poi)):
+      path.start = poi[i]
+      path.end = poi[j]
+      path.generate()
 
   world.layers.append(elevation)
-  world.layers.append(contour)
+  world.layers.append(path)
 
   print_world(scr, world)
   scr.refresh()
